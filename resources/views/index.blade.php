a3f5b32db3446522f8867b6fae30843a48f3b58a
Luizandro Martin <lmartin@xazar.com.mx>
2023-09-09 13:33:25 -0600

    Caroussel Functional

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,29 @@
 @extends('layout')
 
 @section('content')
-    <div class="container">
-        <h1>Mi Página</h1>
-        <!-- Contenido específico de la página -->
+    <div class="carousel-container">
+        <div class="image-container">
+            <div class="image"></div>
+            <div class="image"></div>
+            <div class="image"></div>
+            <!-- <img class="image active" src="https://images.pexels.com/photos/17792525/pexels-photo-17792525/free-photo-of-mano-a-mano.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="">
+            <img class="image" src="https://images.pexels.com/photos/12352425/pexels-photo-12352425.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt="">
+            <img class="image" src="https://images.pexels.com/photos/13438929/pexels-photo-13438929.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" alt=""> -->
+        </div>
+        <div class="navigation-points">
+            <div class="point"></div>
+            <div class="point"></div>
+            <div class="point"></div>
+        </div>
+        <div class="social-media">
+            <div class="social-icon"></div>
+            <div class="social-icon"></div>
+            <div class="social-icon"></div>
+        </div>
+        <div class="nav-buttons">
+            <div class="prev-button"></div>
+            <div class="next-button"></div>
+            <div class="next-label ">Next</div>
+        </div>
     </div>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,12 +9,12 @@
 <body>
     @include('navbar')
 
-    <div class="container">
-        @yield('content')
-    </div>
 
-    @include('footer') <!-- Incluye el footer en todas las páginas -->
-    <script>
+    @yield('content')
+
+
+    @include('footer')
+<script>
     document.addEventListener("DOMContentLoaded", function () {
         const menuContainer = document.querySelector(".menu-container");
         const menuList = document.querySelector(".menu-list");
@@ -29,6 +29,56 @@
         menuList.addEventListener("click", function () {
             menuContainer.classList.remove("menu-open");
         });
+    });
+</script>
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".image");
+    const points = document.querySelectorAll(".point");
+    const prevButton = document.querySelector(".prev-button");
+    const nextButton = document.querySelector(".next-button");
+    let currentIndex = 0;
+
+    // Función para mostrar la imagen activa y el punto activo
+    function showSlide(index) {
+        images.forEach((image, i) => {
+            if (i == index) {
+                image.style.display = "block";
+            } else {
+                image.style.display = "none";
+            }
+        });
+        points.forEach((point, i) => {
+            if (i === index) {
+                point.classList.add("active");
+            } else {
+                point.classList.remove("active");
+            }
+        });
+    }
+
+    // Mostrar la primera imagen al cargar la página
+    showSlide(currentIndex);
+
+    // Botón "Prev" - retroceder a la imagen anterior
+    prevButton.addEventListener("click", function () {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showSlide(currentIndex);
+    });
+
+    // Botón "Next" - avanzar a la siguiente imagen
+    nextButton.addEventListener("click", function () {
+        currentIndex = (currentIndex + 1) % images.length;
+        showSlide(currentIndex);
+    });
+
+    // Hacer clic en los puntos para cambiar la imagen
+    points.forEach((point, index) => {
+        point.addEventListener("click", function () {
+        currentIndex = index;
+        showSlide(currentIndex);
+        });
+    });
     });
 </script>
 </body>
