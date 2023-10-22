2a786716d602e562a029b721782677d7275a2c32
Luizandro Martin <lmartin@xazar.com.mx>
2023-10-22 14:10:35 -0600

    Add register page and menu hamburger fullscreen

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -14,13 +14,44 @@
                 <div class="bar"></div>
                 <div class="bar"></div>
             </div>
-            <ul class="menu-list">
+            <div class="menu-list">
+                <div class="menu-top">
+                    <img src="" alt="">
+                    <div class="close-menu">
+                        <span>CERRAR MENU</span>
+                        <img width="25" height="25" src="https://img.icons8.com/ios-glyphs/30/ffffff/delete-sign.png" alt="delete-sign"/>
+                    </div>
+                </div>
+                <div class="menu-central">
+                    <div class="menu-content">
+                        <p>Publicación</p>
+                        <p class="click-menu" onclick="location.href='{{ url('/register') }}'">Crear tu cuenta</p>
+                        <p class="click-menu">Ingresa</p>
+                        <p class="click-menu">Publica aquí</p>
+                    </div>
+                    <div class="menu-content">
+                        <p>Servicios</p>
+                        <p>Tianguis ganadero</p>
+                        <p class="click-menu">Ganadero genético</p>
+                        <p class="click-menu">Subasta</p>
+                        <p class="click-menu">Noticias</p>
+                    </div>
+                </div>
+                <div class="menu-social">
+                    <div class="social-media--contact-container" style="justify-content: center;">
+                        <img class="social-icon" width="25" height="25" src="https://img.icons8.com/material-sharp/50/ffffff/youtube-play.png" alt="youtube-play"/>
+                        <img class="social-icon" width="25" height="25" src="https://img.icons8.com/windows/50/ffffff/facebook-f--v2.png" alt="facebook-f--v2"/>
+                        <img class="social-icon" width="25" height="25" src="https://img.icons8.com/ios/50/ffffff/instagram-new--v1.png" alt="instagram-new--v1"/>
+                    </div>
+                </div>
+            </div>
+            <!-- <ul class="menu-list">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Tianguis Ganadero</a></li>
                 <li><a href="#">Ganado Genético</a></li>
                 <li><a href="#">Subasta</a></li>
                 <li><a href="#">Noticias</a></li>
-            </ul>
+            </ul> -->
         </div>
     </div>
 </nav>
