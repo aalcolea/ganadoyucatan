f4fded23f4c471e2fdf6c5376e83dbdeaceeff9b
aalcolea <alanalcolea@gmail.com>
2024-03-13 02:42:22 -0600

    routes changes

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::post('/register', [APIAuthController::class, 'register']);
 Route::post('/login', [APIAuthController::class, 'login']);
 Route::post('/newLogin', [APIAuthController::class, 'newLogin']);
 /*get Products methods*/
-Route::get('/productsAll', [APIProductsController::class, 'show']);
+Route::middleware('jwt.auth')->get('/productsAll', [APIProductsController::class, 'show']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     //return $request->user();
 });
