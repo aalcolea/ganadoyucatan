87ae111dcdbf68179c7e12fea818b031c0a8ffe3
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-03-16 12:44:38 -0600

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -66,6 +66,18 @@ return [
         'webp_images_com' => [
         'driver' => 'local',
         'root' => env('LOCAL_UPLOAD_PATH') . '/tianguis'
+        ],        
+        'videos' => [
+        'driver' => 'local',
+        'root' => env('LOCAL_UPLOAD_PATH') . '/videos'
+        ],        
+        'videost' => [
+        'driver' => 'local',
+        'root' => env('LOCAL_UPLOAD_PATH') . '/videost'
+        ],        
+        'videoss' => [
+        'driver' => 'local',
+        'root' => env('LOCAL_UPLOAD_PATH') . '/videoss'
         ],
 
     ],
