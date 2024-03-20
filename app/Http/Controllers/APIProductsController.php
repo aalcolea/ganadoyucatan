0ee54fef51a510ac9b8f64b2a4115f42fbe480f9
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-03-20 12:07:41 -0600

    apis changes

--- app/Http/Controllers/APIProductsController.php
+++ app/Http/Controllers/APIProductsController.php
@@ -13,5 +13,19 @@ class APIProductsController extends Controller
         $products = Product::where('status', '1')->where('vendedorid', $vendedorId)->orderBy('idproducto', 'desc')->paginate(10);
         return response()->json(['products' => $products]);
     }
+    public function getEstados() {
+        $estados = Estado::all();
+        return response()->json($estados);
+    }
+
+    public function getCiudadesByEstado($estadoId) {
+        $ciudades = Ciudad::where('estado_id', $estadoId)->get();
+        return response()->json($ciudades);
+    }
+
+    public function getComisariasByCiudad($ciudadId) {
+        $comisarias = Comisaria::where('ciudad_id', $ciudadId)->get();
+        return response()->json($comisarias);
+    }
 }
 //imagen, titulo, raza, peso, precio, vistas, estatus
