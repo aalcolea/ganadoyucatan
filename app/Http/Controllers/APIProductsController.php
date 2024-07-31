de721c1548483e983c15e641e68a0c759cd23e5a
alanalcolea <alanalcolea@gmail.com>
2024-07-31 01:50:05 -0700

    update edit

--- app/Http/Controllers/APIProductsController.php
+++ app/Http/Controllers/APIProductsController.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\DB;
 use App\Utilities\ImageConverter;
 use Intervention\Image\ImageManagerStatic as Image;
 use App\Models\Video;
-
+use Illuminate\Support\Facades\Log;
 class APIProductsController extends Controller
 {
     public function show(){
@@ -262,99 +262,112 @@ class APIProductsController extends Controller
 
         return response()->json(['message' => 'Producto agregado con éxito'], 200);
     }
-    public function updateGen(Request $request, $id){
-        $data = $request->all();
-        $rules = [
-            'nombre' => 'required',
-            'rancho' => 'required',
-            'descripcion' => 'required',
-        ];
-        $messages = [
-            'nombre.required' => 'El nombre del producto es obligatorio',
-            'rancho.required' => 'El rancho es obligatorio',
-            'descripcion.required' => 'La descripción es obligatoria',
-        ];
-        $validator = Validator::make($data, $rules, $messages);
 
-        if ($validator->fails()) {
-            return response()->json(['errors' => $validator->errors()], 422);
-        }
+public function updateGen(Request $request, $id) {
+    Log::info('Entrando en updateGen');
+    $data = $request->all();
+    Log::info('Datos recibidos:', $data);
 
-        try {
-            $product = Product::find($id);
-            if (!$product) {
-                return response()->json(['error' => 'Producto no encontrado'], 404);
-            }
+    $rules = [
+        'nombre' => 'required',
+        'rancho' => 'required',
+        'descripcion' => 'required',
+    ];
+    $messages = [
+        'nombre.required' => 'El nombre del producto es obligatorio',
+        'rancho.required' => 'El rancho es obligatorio',
+        'descripcion.required' => 'La descripción es obligatoria',
+    ];
+    $validator = Validator::make($data, $rules, $messages);
 
-            $nombre = $data['nombre'];
-            $ruta = strtolower(str_replace(" ", "-", $nombre));
-            $product->nombre = $nombre;
-            $product->descripcion = $data['descripcion'];
-            $product->rancho = $data['rancho'];
-            $product->raza = $data['raza'];
-            $product->ruta = $ruta;
-            $product->precio = $data['precio'];
-            $product->edad = $data['edad'];
-            $product->stock = $data['stock'];
-            $product->vacunado = $data['vacu'];
-            $product->peso = $data['peso'];
-            $product->estado = $data['estado'];
-            $product->ciudad = $data['ciudad'];
-            $product->comisaria = $data['comisaria'] ?? $product->comisaria;
-            $product->save();
-
-            if ($request->hasFile('images')) {
-                $images = $request->file('images');
-                $dateFolder = $product->carpeta ?: date('Y-m-d');
-                $uploadPath = 'uploads/' . $dateFolder;
-                if (!File::exists($uploadPath)) {
-                    File::makeDirectory($uploadPath, 0755, true);
-                }
-                foreach ($images as $index => $image) {
-                    $filename = $image->getClientOriginalName();
-                    $webpPath = $dateFolder . '/' . pathinfo($filename, PATHINFO_FILENAME) . '.webp';
-                    $destinationPath = Storage::disk('webp_images')->path($webpPath);
-
-                    try {
-                        $img = Image::make($image->getRealPath());
-                        $img->encode('webp', 10)->save($destinationPath);
-                    } catch (Exception $e) {
-                        return response()->json(['error' => 'No se pudo guardar la imagen: ' . $e->getMessage()], 500);
-                    }
-
-                    if ($index === 0) {
-                        $product->portada = pathinfo($filename, PATHINFO_FILENAME) . '.webp';
-                        $product->carpeta = $dateFolder;
-                        $product->save();
-                    }
-
-                    $imageEntry = new PGallery;
-                    $imageEntry->productoid = $product->idproducto;
-                    $imageEntry->img = $filename;
-                    $imageEntry->save();
-                }
-            }
-            if ($request->hasFile('videos')) {
-                $videos = $request->file('videos');
-
-                foreach ($videos as $videoFile) {
-                    $destinationPath = Storage::disk('videos')->path($videoFile);
-                    $videoPath = $videoFile->store('', 'videos');
-
-                    $video = new Video();
-                    $video->nombre = $videoFile->getClientOriginalName();
-                    $video->ruta = $videoPath;
-                    $video->tamaño = $videoFile->getSize();
-                    $video->producto_id = $product->idproducto;
-                    $video->save();
-                }
-            }
-
-            return response()->json(['message' => 'Producto actualizado con éxito'], 200);
-        } catch (Exception $e) {
-            return response()->json(['error' => 'Error al actualizar producto: ' . $e->getMessage(), 'trace' => $e->getTraceAsString()], 500);
-        }
+    if ($validator->fails()) {
+        Log::info('Validación fallida:', $validator->errors());
+        return response()->json(['errors' => $validator->errors()], 422);
     }
+
+    try {
+        $product = Product::find($id);
+        if (!$product) {
+            Log::info('Producto no encontrado');
+            return response()->json(['error' => 'Producto no encontrado'], 404);
+        }
+
+        $nombre = $data['nombre'];
+        $ruta = strtolower(str_replace(" ", "-", $nombre));
+        $product->nombre = $nombre;
+        $product->descripcion = $data['descripcion'];
+        $product->rancho = $data['rancho'];
+        $product->raza = $data['raza'];
+        $product->ruta = $ruta;
+        $product->precio = $data['precio'];
+        $product->edad = $data['edad'];
+        $product->stock = $data['stock'];
+        $product->vacunado = $data['vacu'];
+        $product->peso = $data['peso'];
+        $product->estado = $data['estado'];
+        $product->ciudad = $data['ciudad'];
+        $product->comisaria = $data['comisaria'] ?? $product->comisaria;
+        $product->save();
+
+        if ($request->hasFile('images')) {
+            Log::info('Procesando imágenes');
+            $images = $request->file('images');
+            $dateFolder = $product->carpeta ?: date('Y-m-d');
+            $uploadPath = 'uploads/' . $dateFolder;
+            if (!File::exists($uploadPath)) {
+                File::makeDirectory($uploadPath, 0755, true);
+            }
+            foreach ($images as $index => $image) {
+                $filename = $image->getClientOriginalName();
+                $webpPath = $dateFolder . '/' . pathinfo($filename, PATHINFO_FILENAME) . '.webp';
+                $destinationPath = Storage::disk('webp_images')->path($webpPath);
+
+                try {
+                    $img = Image::make($image->getRealPath());
+                    $img->encode('webp', 10)->save($destinationPath);
+                } catch (Exception $e) {
+                    Log::error('Error al guardar la imagen: ' . $e->getMessage());
+                    return response()->json(['error' => 'No se pudo guardar la imagen: ' . $e->getMessage()], 500);
+                }
+
+                if ($index === 0) {
+                    $product->portada = pathinfo($filename, PATHINFO_FILENAME) . '.webp';
+                    $product->carpeta = $dateFolder;
+                    $product->save();
+                }
+
+                $imageEntry = new PGallery;
+                $imageEntry->productoid = $product->idproducto;
+                $imageEntry->img = $filename;
+                $imageEntry->save();
+            }
+        }
+
+        if ($request->hasFile('videos')) {
+            Log::info('Procesando videos');
+            $videos = $request->file('videos');
+
+            foreach ($videos as $videoFile) {
+                $destinationPath = Storage::disk('videos')->path($videoFile);
+                $videoPath = $videoFile->store('', 'videos');
+
+                $video = new Video();
+                $video->nombre = $videoFile->getClientOriginalName();
+                $video->ruta = $videoPath;
+                $video->tamaño = $videoFile->getSize();
+                $video->producto_id = $product->idproducto;
+                $video->save();
+            }
+        }
+
+        Log::info('Producto actualizado con éxito');
+        return response()->json(['message' => 'Producto actualizado con éxito'], 200);
+    } catch (Exception $e) {
+        Log::error('Error al actualizar producto: ' . $e->getMessage());
+        return response()->json(['error' => 'Error al actualizar producto: ' . $e->getMessage(), 'trace' => $e->getTraceAsString()], 500);
+    }
+}
+
 
 
 }
